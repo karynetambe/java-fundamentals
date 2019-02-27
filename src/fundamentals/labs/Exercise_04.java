487fabaeb6d469d243e4fe5d8250e50c014cfd9f
karyne tambe <ktabot.tambe@gmail.com>
2019-02-26 23:16:58 -0600

    pushing example lab

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -12,7 +12,12 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        // write code here
+        System.out.println("Print out the result!");
+
+        // write code to print out "Calculate the volume and surface area of a cylinder with aa radius of 3.14 and a height of 5!" below
+        System.out.print ("Print out the result");
+
+
 
     }
 }
